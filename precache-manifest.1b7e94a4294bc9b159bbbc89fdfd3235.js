87972ba06560633f7df184a7322f05e0331fb15d
PunithAchar <punithachar14@gmail.com>
2021-03-30 14:48:32 +0530

    Updates

---
+++ precache-manifest.1b7e94a4294bc9b159bbbc89fdfd3235.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a200683cfa884e4b82281f823e3287e2",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "221fd50f921234da8247",
+    "url": "/keeper/static/js/2.f62d1186.chunk.js"
+  },
+  {
+    "revision": "5dc79b02922b7bc60deb",
+    "url": "/keeper/static/js/main.29b7239c.chunk.js"
+  },
+  {
+    "revision": "abfa8085494f45f6d743",
+    "url": "/keeper/static/js/runtime-main.0b695617.js"
+  }
+]);
